6c5f3e6cebbf3b641887c4413241e60e3b93d1d4
Shreesha Aithal <9211shreesha@gmail.com>
2016-02-24 10:35:39 +0530

    Add background images to other pages

--- scripts-common/wallpaper.js
+++ scripts-common/wallpaper.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
   var n = d.getHours();
   if (n > 20 || n < 6) {
   // If time is after 7PM or before 6AM, apply night theme to ‘body’
-    document.body.className = "night";
+    document.getElementById("background-image").className = "night";
   } else if (n > 16 && n < 20) {
   // If time is between 4PM – 7PM sunset theme to ‘body’
-    document.body.className = "sunset";
+    document.getElementById("background-image").className = "sunset";
   } else if (n > 6 || n < 10) {
     // Else use ‘day’ theme
-    document.body.className = "morning";
+    document.getElementById("background-image").className = "morning";
   } else 
     // use the 'road trip background'
-    document.body.className = "roadtrip";
+    document.getElementById("background-image").className = "roadtrip";
 });
