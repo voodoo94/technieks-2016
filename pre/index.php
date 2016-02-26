4fd1f6f1698d816900349a340d62e36d6c7641ab
Shreesha Aithal <9211shreesha@gmail.com>
2016-02-26 17:42:15 +0530

    A lot of changes
    Added new fonts, hover animations, structured the code better, added htc
    as title sponsor (commented), removed unused content

--- pre/index.php
+++ pre/index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-<title>Scoreboard</title>
+<title>Pre-techNIEks | Scoreboard</title>
 <script src="js/jquery.min.js"></script>
 <link href="./style@technieks.css" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" href="nie_logo_favicon.png">
 <style>
 body {
   background-color: #fe9;
--- scripts-common/wallpaper.js
+++ scripts-common/wallpaper.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
   var d = new Date();
   var n = d.getHours();
-  if (n > 21 && n < 6) {
-    document.getElementById("background-image").className = "night";
-  } else if (n > 16 && n < 20) {
-    document.getElementById("background-image").className = "sunset";
-  } else if (n > 6 && n < 10) {
+  if (n > 6 && n <=10) {
     document.getElementById("background-image").className = "morning";
-  } else {
+  } else if (n > 10 && n <= 16) {
     document.getElementById("background-image").className = "roadtrip";
+
     var list = document.getElementsByClassName("con_images");
     var i;
     for (i = 0; i < list.length; i++) {
       list[i].style.color = "#FFF";
     }
-
+  } else if (n > 16 && n <= 20) {
+    document.getElementById("background-image").className = "sunset";
+  } else {
+    document.getElementById("background-image").className = "night";
   }
 });
